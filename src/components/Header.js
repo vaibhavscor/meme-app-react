3c5b5f0a3793707d337b8d8ba39ddff4f47db27e
scorpion <vaibhav.159@gmail.com>
2020-05-31 02:43:27 +0530

    first complete app commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+        <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="meme"
+        />
+        <p>MEME GENERATOR</p>
+        </header>
+    )
+}
